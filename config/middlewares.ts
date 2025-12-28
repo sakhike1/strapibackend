637da25d126d83a95a2dd6696adcb3e44efeb35c
sakhike1 <sakhike1@github.com>
2025-12-28 19:24:56 +0200

    Add localhost:5174 to CORS configuration

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -10,6 +10,7 @@ export default [
         'http://localhost:3000',
         'http://localhost:3001',
         'http://localhost:5173',
+        'http://localhost:5174',
         // Production URLs
         'https://skipaman.co.za',
         'https://www.skipaman.co.za',
